b095cea18728686c278052d359f6ca12e0b47668
Ananya Jain <115038721+theananyajain@users.noreply.github.com>
2023-05-13 23:56:06 +0530

    first commit

---
+++ stopwatch.js
@@ -0,0 +1,89 @@
+let timerDisplay = document.querySelector(".display");
+let startbtn = document.querySelector(".start");
+let stopbtn = document.querySelector(".stop");
+let resetbtn = document.querySelector(".reset");
+
+// let msec = 00;
+// let secs = 00;
+// let mins = 00;
+
+// let timerId = null;
+
+// startbtn.addEventListener('click', function(){
+//     if(timerId !== null){
+//         clearInterval(timerId);
+//     }
+//     timerId = setInterval(startTimer, 10);
+// });
+
+// stopbtn.addEventListener('click', function(){
+//     clearInterval(timerId);
+// });
+
+// resetbtn.addEventListener('click', function(){
+//     clearInterval(timerId);
+//     timerDisplay.innerHTML = `00 : 00 : 00`;
+//     msec = secs = mins = 00;
+// });
+
+// function startTimer(){
+//     msec++;
+//     if(msec == 100){
+//         msec = 0;
+//         secs++;
+//         if(secs == 60){
+//             secs = 0;
+// mins++;
+//         }
+//     }
+
+//     let msecString = msec < 10 ? `0${msec}` : msec;
+//     let secsString = secs < 10 ? `0${secs}` : secs;
+//     let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+//     timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
+// }
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+
+startbtn.addEventListener("click", function () {
+  if (timerId !== null) {
+    clearInterval(timerId);
+  }
+  timerId = setInterval(startTimer, 10);
+});
+
+stopbtn.addEventListener("click", function () {
+  clearInterval(timerId);
+});
+resetbtn.addEventListener("click", function () {
+  clearInterval(timerId);
+  timerDisplay.innerHTML = `00 : 00 : 00`;
+  msec = secs = mins = 00;
+});
+
+
+function startTimer() {
+  msec++;
+  if (msec == 100) {
+    msec = 0;
+    secs++;
+    if (secs == 60) {
+      secs = 0;
+      mins++;
+    }
+  }
+
+
+let msecString = msec < 10 ? `0${msec}` : msec;
+let secString = secs < 10 ? `0${secs}` : secs;
+let minString = mins < 10 ? `0${mins}` : mins;
+
+timerDisplay.innerHTML = `${minString} : ${secString} : ${msecString}`;
+}
